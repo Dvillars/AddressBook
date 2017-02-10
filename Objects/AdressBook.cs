a6cb4fe8a69b1952fbf68f4cbf5e1ee79cd77ef8
Derek Villars <Derekvillars@yahoo.com>
2017-02-10 09:41:01 -0800

    add getters and setters

--- Objects/AdressBook.cs
+++ Objects/AdressBook.cs
@@ -2,8 +2,40 @@ using System.Collections.Generic;
 
 namespace AdressBook.Objects
 {
-    public class Address
+    public class Contact
     {
+        private static List<Contact> Contactlist = new List<Contact> {};
+
+        private string _name;
+        private string _phoneNumber;
+        private string _address;
+
+        public string GetName()
+        {
+          return _name;
+        }
+        public void SetName(string newName)
+        {
+          _name = newName;
+        }
+
+        public string GetPhoneNumber()
+        {
+          return _phoneNumber;
+        }
+        public void SetPhoneNumber(string newPhoneNumber)
+        {
+          _phoneNumber = newPhoneNumber;
+        }
+
+        public string GetAddress()
+        {
+          return _address;
+        }
+        public void SetAddress(string newAddress)
+        {
+          _address = newAddress;
+        }
 
     }
 }
