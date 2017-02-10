002ea40b4019df58f39bb5db19cdadc6352ae21c
Derek Villars <Derekvillars@yahoo.com>
2017-02-10 10:08:51 -0800

    change the setters and homemodule link things

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -4,26 +4,37 @@ using AdressBook.Objects;
 
 namespace AdressBook
 {
-  public class HomeModule : NancyModule
-  {
-    public HomeModule()
+    public class HomeModule : NancyModule
     {
-      Get["/"] = _ =>
-      {
-        return View["index.cshtml"];
-      };
-      Get["/contacts/view"] = _ =>
-      {
-        return View["index.cshtml"];
-      };
-      Get["/contacts/new"] = _ =>
-      {
-        return View["index.cshtml"];
-      };
-      Get["/contacts/clear"] = _ =>
-      {
-        return View["index.cshtml"];
-      };
+        public HomeModule()
+        {
+          Get["/"] = _ =>
+          {
+              if (Job.GetCounter() == 0)
+              {
+                return View["none.cshtml"];
+              }
+              else
+              {
+                return View["index.cshtml", Job.JobList()];
+              }
+          };
+
+          Get["/contacts/view/{id}"] = parameters =>
+          {
+              return View["view.cshtml"];
+          };
+
+          Post["/contacts/new"] = _ =>
+          {
+            Job newJob = new Job(Request.Form["job-name"], Request.Form["job-description"], Request.Form["job-contact"]);
+            return View["new.cshtml"];
+          };
+
+          Get["/contacts/clear"] = _ =>
+          {
+              return View["clear.cshtml"];
+          };
+        }
     }
-  }
 }
--- Objects/AdressBook.cs
+++ Objects/AdressBook.cs
@@ -4,38 +4,44 @@ namespace AdressBook.Objects
 {
     public class Contact
     {
-        private static List<Contact> Contactlist = new List<Contact> {};
-
         private string _name;
         private string _phoneNumber;
         private string _address;
+        private static List<Contact> Contactlist = new List<Contact> {};
+        private static int _counter = 0;
+
+        public Contact(string Name, string PhoneNumber, string Address)
+        {
+          _name = Name;
+          _phoneNumber = PhoneNumber;
+          _address = Address;
+          _instances.Add(this);
+          _counter++;
+        }
 
         public string GetName()
         {
-          return _name;
-        }
-        public void SetName(string newName)
-        {
-          _name = newName;
+            return _name;
         }
 
         public string GetPhoneNumber()
         {
-          return _phoneNumber;
-        }
-        public void SetPhoneNumber(string newPhoneNumber)
-        {
-          _phoneNumber = newPhoneNumber;
+            return _phoneNumber;
         }
 
         public string GetAddress()
         {
-          return _address;
-        }
-        public void SetAddress(string newAddress)
-        {
-          _address = newAddress;
+            return _address;
         }
 
+        public static List<Contact> ContactList()
+        {
+            return _instances;
+        }
+
+        public static int GetCounter()
+        {
+            return _counter;
+        }
     }
 }
